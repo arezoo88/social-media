c8ae494f1a35acd67298f811e7b02954b53abb6a
Arezoo <arezoo.darvishi69@yahoo.com>
2022-08-12 16:57:58 +0430

    feat: create profile model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from cloudinary.models import CloudinaryField
+from mapbox_location_field.models import LocationField
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profile_image = CloudinaryField('image')
+    location = models.CharField(max_length=100, blank=False)
+
+    def __str__(self) -> str:
+        return self.user.username
