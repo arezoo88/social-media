deec7e7e6a54bc12a2891fdbc91d2ef5f1de8f5f
Arezoo <arezoo.darvishi69@yahoo.com>
2022-08-20 10:03:36 +0430

    feat: create url for signin

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,10 @@ import imp
 from urllib.parse import urlparse
 from django.urls import path
 from . import views
+app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup')
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin')
 ]
